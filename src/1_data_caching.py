b6079665d9fb379849ea30f30cb39145baa0dc04
Hanrui Lyu <hlyu@dt-login03.delta.ncsa.illinois.edu>
2025-02-10 15:19:51 -0600

    decode single session for unaligned data

--- src/1_data_caching.py
+++ src/1_data_caching.py
@@ -1,3 +1,4 @@
+# %%
 import os
 import sys
 import argparse
@@ -12,30 +13,41 @@ from utils.ibl_data_utils import (
     list_brain_regions, 
     bin_spiking_data,
     bin_behaviors,
-    align_spike_behavior
+    align_spike_behavior,
+    create_intervals, 
 )
 from utils.dataset_utils import create_dataset, upload_dataset
 
+# %%
 from reproducible_ephys_functions import filter_recordings
 from fig_PCA.fig_PCA_load_data import load_dataframe
 
+# %%
 ap = argparse.ArgumentParser()
-ap.add_argument("--base_path", type=str, default="EXAMPLE_PATH")
-ap.add_argument("--fold_idx", type=int, default=1)
+ap.add_argument("--base_path", type=str, default="/scratch/bcxj/hlyu/RRR/")
+ap.add_argument("--fold_idx", type=int, default=10)
 ap.add_argument("--n_workers", type=int, default=1)
 args = ap.parse_args()
+
+# args = argparse.Namespace(
+#     base_path="/scratch/bcxj/hlyu/RRR/",
+#     fold_idx=10,
+#     n_workers=1
+# )
+# %%
 
 SEED = 42
 np.random.seed(SEED)
 assert args.fold_idx > 0, "Fold idx must be from 1 to 5."
 
+# %%
 one = ONE(
     base_url='https://openalyx.internationalbrainlab.org', 
     password='international', silent=True,
     cache_dir = args.base_path
 )
 
-freeze_file = 'data/bwm_release.csv'
+freeze_file = '/u/hlyu/neural_decoding/data/bwm_release.csv'
 bwm_df = pd.read_csv(freeze_file, index_col=0)
 
 concat_df = load_dataframe()
@@ -47,13 +59,21 @@ params = {
     'align_time': 'stimOn_times', 'time_window': (-.5, 1.5)
 }
 
-beh_names = ['choice', 'reward', 'stimside', 'wheel-speed', 'whisker-motion-energy']
+beh_names = ['wheel-speed', 'whisker-motion-energy', 'pupil-diameter'] # Decode continuous behavioral variables (wheel speed, whisker motion energy, pupil diameter)
 
-include_eids = np.unique(concat_df.eid)
+# include_eids = np.unique(concat_df.eid)
+include_eids = ['d0ea3148-948d-4817-94f8-dcaf2342bbbe',
+    'd23a44ef-1402-4ed7-97f5-47e9a7a504d9',
+    'd2832a38-27f6-452d-91d6-af72d794136c',
+    'd57df551-6dcb-4242-9c72-b806cff5613a',
+    'dac3a4c1-b666-4de0-87e8-8c514483cacf'
+    ]
+
 bad_eids = []
 
 print(f"Preprocess a total of {len(include_eids)} EIDs.")
 
+# %%
 num_neurons = []
 for eid_idx, eid in enumerate(include_eids):
 
@@ -64,72 +84,52 @@ for eid_idx, eid in enumerate(include_eids):
     print(f'Preprocess session {eid}:')
 
     # Load and preprocess data
-    neural_dict, behave_dict, meta_data, trials_data = prepare_data(one, eid, bwm_df, params, n_workers=args.n_workers)
-    regions, beryl_reg = list_brain_regions(neural_dict, **params)
+    # neural_dict, behave_dict, meta_data, trials_data = prepare_data(one, eid, bwm_df, params, n_workers=args.n_workers)
+    # print('neural_dict has ', neural_dict.keys())
+    # regions, beryl_reg = list_brain_regions(neural_dict, **params)
    
+    # region_cluster_ids = select_brain_regions(neural_dict, beryl_reg, regions, **params)
+    # print(region_cluster_ids)
+    # binned_spikes, clusters_used_in_bins = bin_spiking_data(
+    #     region_cluster_ids, neural_dict, trials_df=trials_data['trials_df'], n_workers=args.n_workers, **params
+    # )
+
+    #####################################################
+    neural_dict, _, meta_data, _ = prepare_data(one, eid, bwm_df, params, n_workers=args.n_workers)
+    regions, beryl_reg = list_brain_regions(neural_dict, **params)
     region_cluster_ids = select_brain_regions(neural_dict, beryl_reg, regions, **params)
-    binned_spikes, clusters_used_in_bins = bin_spiking_data(
-        region_cluster_ids, neural_dict, trials_df=trials_data['trials_df'], n_workers=args.n_workers, **params
+    intervals = create_intervals(
+        start_time=0, end_time=neural_dict['spike_times'].max(), interval_len=params['interval_len']
     )
+    binned_spikes, clusters_used_in_bins = bin_spiking_data(
+        region_cluster_ids, neural_dict, intervals=intervals, n_workers=3, **params
+    )
+
     try:
         binned_behaviors, behavior_masks = bin_behaviors(
-            one, eid, beh_names[3:], trials_df=trials_data['trials_df'], 
+            one, eid, beh_names, intervals=intervals, ## trials_df=trials_data['trials_df'], 
             allow_nans=True, n_workers=args.n_workers, **params
         ) 
         # Ensure neural and behavior data match for each trial
         aligned_binned_spikes, aligned_binned_behaviors = align_spike_behavior(
-            binned_spikes, binned_behaviors, beh_names, trials_data['trials_mask']
+            binned_spikes, binned_behaviors, beh_names, ##trials_data['trials_mask']
         )
     except ValueError as e:
         print(e)
         continue
 
-    print("spike data shape: ", aligned_binned_spikes.shape)
-    num_neurons.append(aligned_binned_spikes.shape[-1])
-
     # Partition dataset (train: 0.7 val: 0.1 test: 0.2)
     max_num_trials = len(aligned_binned_spikes)
     trial_idxs = np.random.choice(np.arange(max_num_trials), max_num_trials, replace=False)
-    
-    num_folds = 5
-    fold_size = len(trial_idxs) // num_folds
-    folds = [trial_idxs[i * fold_size:(i + 1) * fold_size] for i in range(num_folds)]
-     
-    for fold in range(num_folds):
+    train_idxs = trial_idxs[:int(0.7*max_num_trials)]
+    val_idxs = trial_idxs[int(0.7*max_num_trials):int(0.8*max_num_trials)]
+    test_idxs = trial_idxs[int(0.8*max_num_trials):]
 
-        if (fold+1) != args.fold_idx:
-            continue
-
-        test_idxs = folds[fold]
-
-        train_val_idxs = np.concatenate([folds[i] for i in range(num_folds) if i != fold])
-
-        num_train = int(0.875 * len(train_val_idxs))  # 70% of the total data
-        train_idxs = train_val_idxs[:num_train]
-        val_idxs = train_val_idxs[num_train:]
-
-        print(f"Fold {fold + 1}:")
-
-    if len(train_idxs) == 0 or len(val_idxs) == 0 or len(test_idxs) == 0:
-        print(f"Skip {eid} due to empty set.")
-        continue
-
-    is_cls_balance = True
     train_beh, val_beh, test_beh = {}, {}, {}
     for beh in aligned_binned_behaviors.keys():
         train_beh.update({beh: aligned_binned_behaviors[beh][train_idxs]})
         val_beh.update({beh: aligned_binned_behaviors[beh][val_idxs]})
         test_beh.update({beh: aligned_binned_behaviors[beh][test_idxs]})
-
-        if beh in ["choice", "stimside", "reward"]:
-            if any(len(np.unique(beh_data)) < 2 for beh_data in \
-                    [train_beh[beh], val_beh[beh], test_beh[beh]]):
-                is_cls_balance = False
-                break
-    
-    if not is_cls_balance:
-        print(f"Skip {eid} due to imbalanced cls distribution.")
-        continue
     
     train_dataset = create_dataset(
         aligned_binned_spikes[train_idxs], bwm_df, eid, params, 
@@ -153,13 +153,93 @@ for eid_idx, eid in enumerate(include_eids):
     print(partitioned_dataset)
 
     # Cache dataset
-    save_path = Path(args.base_path)/'cached_re_data'/f'fold_{args.fold_idx}'
+    save_path = Path(args.base_path)/'cached_ibl_data'
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     partitioned_dataset.save_to_disk(f'{save_path}/{eid}')
 
     print(f'Cached session {eid}.')
     print(f'Progress: {eid_idx+1} / {len(include_eids)} sessions cached.')
+            
+
+    print("spike data shape: ", aligned_binned_spikes.shape)
+    num_neurons.append(aligned_binned_spikes.shape[-1])
+
+    # # Partition dataset (train: 0.7 val: 0.1 test: 0.2)
+    # max_num_trials = len(aligned_binned_spikes)
+    # trial_idxs = np.random.choice(np.arange(max_num_trials), max_num_trials, replace=False)
+    
+    # num_folds = 5
+    # fold_size = len(trial_idxs) // num_folds
+    # folds = [trial_idxs[i * fold_size:(i + 1) * fold_size] for i in range(num_folds)]
+     
+    # for fold in range(num_folds):
+
+    #     if (fold+1) != args.fold_idx:
+    #         continue
+
+    #     test_idxs = folds[fold]
+
+    #     train_val_idxs = np.concatenate([folds[i] for i in range(num_folds) if i != fold])
+
+    #     num_train = int(0.875 * len(train_val_idxs))  # 70% of the total data
+    #     train_idxs = train_val_idxs[:num_train]
+    #     val_idxs = train_val_idxs[num_train:]
+
+    #     print(f"Fold {fold + 1}:")
+
+    # if len(train_idxs) == 0 or len(val_idxs) == 0 or len(test_idxs) == 0:
+    #     print(f"Skip {eid} due to empty set.")
+    #     continue
+
+    # is_cls_balance = True
+    # train_beh, val_beh, test_beh = {}, {}, {}
+    # for beh in aligned_binned_behaviors.keys():
+    #     train_beh.update({beh: aligned_binned_behaviors[beh][train_idxs]})
+    #     val_beh.update({beh: aligned_binned_behaviors[beh][val_idxs]})
+    #     test_beh.update({beh: aligned_binned_behaviors[beh][test_idxs]})
+
+    #     if beh in ["choice", "stimside", "reward"]:
+    #         if any(len(np.unique(beh_data)) < 2 for beh_data in \
+    #                 [train_beh[beh], val_beh[beh], test_beh[beh]]):
+    #             is_cls_balance = False
+    #             break
+    
+    # if not is_cls_balance:
+    #     print(f"Skip {eid} due to imbalanced cls distribution.")
+    #     continue
+    
+    # train_dataset = create_dataset(
+    #     aligned_binned_spikes[train_idxs], bwm_df, eid, params, 
+    #     binned_behaviors=train_beh, meta_data=meta_data
+    # )
+    # val_dataset = create_dataset(
+    #     aligned_binned_spikes[val_idxs], bwm_df, eid, params, 
+    #     binned_behaviors=val_beh, meta_data=meta_data
+    # )
+    # test_dataset = create_dataset(
+    #     aligned_binned_spikes[test_idxs], bwm_df, eid, params, 
+    #     binned_behaviors=test_beh, meta_data=meta_data
+    # )
+
+    # # Create dataset
+    # partitioned_dataset = DatasetDict({
+    #     'train': train_dataset,
+    #     'val': val_dataset,
+    #     'test': test_dataset}
+    # )
+    # print(partitioned_dataset)
+
+    # # Cache dataset
+    # save_path = Path(args.base_path)/'cached_re_data'/f'fold_{args.fold_idx}'
+    # if not os.path.exists(save_path):
+    #     os.makedirs(save_path)
+    # partitioned_dataset.save_to_disk(f'{save_path}/{eid}')
+
+    # print(f'Cached session {eid}.')
+    # print(f'Progress: {eid_idx+1} / {len(include_eids)} sessions cached.')
 
 print(f"Min: {min(num_neurons)} max: {max(num_neurons)} mean: {sum(num_neurons)/len(num_neurons)} # of neurons.")
 
+
+# %%
---
+++ src/1_decode_single_session.py
@@ -0,0 +1,203 @@
+"""Example script for running single-session reduced-rank model with hyperparameter sweep.
+"""
+import os
+import argparse
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import Ridge, LogisticRegression
+import torch
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch import Trainer
+from ray import tune
+from ray.train.lightning import (
+    RayDDPStrategy,
+    RayLightningEnvironment,
+    RayTrainReportCallback,
+    prepare_trainer,
+)
+from utils.data_loader_utils import SingleSessionDataModule
+from models.decoders import ReducedRankDecoder, MLPDecoder, LSTMDecoder
+from utils.eval_utils import eval_model
+from utils.sweep_utils import tune_decoder
+from utils.utils import set_seed
+from utils.config_utils import config_from_kwargs, update_config
+
+"""
+-----------
+USER INPUTS
+-----------
+"""
+ap = argparse.ArgumentParser()
+ap.add_argument("--eid", type=str)
+ap.add_argument(
+    "--target", type=str, default="choice", 
+    choices=["choice", "wheel-speed", "whisker-motion-energy", "pupil-diameter"]
+)
+ap.add_argument("--region", type=str, default="all")
+ap.add_argument("--method", type=str, default="linear", choices=["linear", "reduced_rank", "mlp", "lstm"])
+ap.add_argument("--n_workers", type=int, default=1)
+ap.add_argument("--base_path", type=str, default="/scratch/bcxj/hlyu/RRR/")
+args = ap.parse_args()
+
+
+"""
+-------
+CONFIGS
+-------
+"""
+kwargs = {"model": "include:src/configs/decoder.yaml"}
+config = config_from_kwargs(kwargs)
+config = update_config("configs/decoder.yaml", config)
+
+if args.target in ["wheel-speed", "whisker-motion-energy", "pupil-diameter"]:
+    config = update_config("src/configs/reg_trainer.yaml", config)
+elif args.target in ['choice']:
+    config = update_config("src/configs/clf_trainer.yaml", config)
+else:
+    raise NotImplementedError
+
+set_seed(config.seed)
+
+config["dirs"]["data_dir"] = Path(args.base_path)/config.dirs.data_dir
+save_path = Path(args.base_path)/config.dirs.output_dir/args.target/args.method/args.region
+ckpt_path = Path(args.base_path)/config.dirs.checkpoint_dir/args.target/args.method/args.region 
+os.makedirs(save_path, exist_ok=True)
+os.makedirs(ckpt_path, exist_ok=True)
+
+model_class = args.method
+
+print('---------------------------------------------')
+print(f'Decode {args.target} from session: {args.eid}')
+
+
+"""
+--------
+DECODING
+--------
+"""
+print('---------------------------------------------')
+print(f'Launch single-session {model_class} decoder:')
+
+# set up model configs
+search_space = config.copy()
+search_space['eid'] = args.eid
+search_space['target'] = args.target
+search_space['region'] = args.region if args.region != 'all' else None
+search_space['training']['device'] = torch.device(
+    'cuda' if np.logical_and(torch.cuda.is_available(), config.training.device == 'gpu') else 'cpu'
+)
+
+# set up for hyperparameter sweep
+if model_class == "linear":
+    dm = SingleSessionDataModule(search_space)
+    dm.setup()
+    if config.model.target == 'reg':
+        model = GridSearchCV(Ridge(), {"alpha": [1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1]})
+    elif config.model.target == 'clf':
+        model = GridSearchCV(LogisticRegression(), {"C": [1, 1e1, 1e2, 1e3, 1e4]})
+    else:
+        raise NotImplementedError
+    
+else:
+    search_space['optimizer']['lr'] = tune.grid_search([1e-2, 1e-3])
+    search_space['optimizer']['weight_decay'] = tune.grid_search([1, 1e-1, 1e-2, 1e-3])
+    
+    if model_class == "reduced_rank":
+        search_space['reduced_rank']['temporal_rank'] = tune.grid_search([2, 5, 10, 15])
+        search_space['tuner']['num_epochs'] = 500
+        search_space['training']['num_epochs'] = 800
+    elif model_class == "lstm":
+        search_space['lstm']['lstm_hidden_size'] = tune.grid_search([128, 64])
+        search_space['lstm']['lstm_n_layers'] = tune.grid_search([1, 3, 5])
+        search_space['lstm']['drop_out'] = tune.grid_search([0., 0.2, 0.4, 0.6])
+        search_space['tuner']['num_epochs'] = 250
+        search_space['training']['num_epochs'] = 250
+    elif model_class == "mlp":
+        search_space['mlp']['drop_out'] = tune.grid_search([0., 0.2, 0.4, 0.6])
+        search_space['tuner']['num_epochs'] = 250
+        search_space['training']['num_epochs'] = 250
+    else:
+        raise NotImplementedError
+
+    def train_func(config):
+        dm = SingleSessionDataModule(config)
+        dm.setup()
+        if model_class == "reduced_rank":
+            model = ReducedRankDecoder(dm.config)
+        elif model_class == "lstm":
+            model = LSTMDecoder(dm.config)
+        elif model_class == "mlp":
+            model = MLPDecoder(dm.config)
+        else:
+            raise NotImplementedError
+            
+        trainer = Trainer(
+            max_epochs=config['tuner']['num_epochs'],
+            devices="auto",
+            accelerator="auto",
+            strategy=RayDDPStrategy(),
+            callbacks=[RayTrainReportCallback()],
+            plugins=[RayLightningEnvironment()],
+            enable_progress_bar=config['tuner']['enable_progress_bar'],
+        )
+        trainer = prepare_trainer(trainer)
+        trainer.fit(model, datamodule=dm)
+    
+    # hyperparameter sweep
+    results = tune_decoder(
+        train_func, search_space, save_dir=ckpt_path,
+        use_gpu=config.tuner.use_gpu, max_epochs=config.tuner.num_epochs, 
+        num_samples=config.tuner.num_samples, num_workers=args.n_workers
+    )
+    best_result = results.get_best_result(metric=config.tuner.metric, mode=config.tuner.mode)
+    best_config = best_result.config['train_loop_config']
+
+    print("Best model config:")
+    print(best_config)
+    
+    # set up trainer
+    checkpoint_callback = ModelCheckpoint(
+        monitor=config.training.metric, mode=config.training.mode, dirpath=ckpt_path
+    )
+    trainer = Trainer(
+        max_epochs=config.training.num_epochs, 
+        callbacks=[checkpoint_callback], 
+        enable_progress_bar=config.training.enable_progress_bar
+    )
+
+    # set up data loader
+    dm = SingleSessionDataModule(best_config)
+    dm.setup()
+
+    # init and train model
+    if model_class == "reduced_rank":
+        model = ReducedRankDecoder(best_config)
+    elif model_class == "lstm":
+        model = LSTMDecoder(best_config)
+    elif model_class == "mlp":
+        model = MLPDecoder(best_config)
+    else:
+        raise NotImplementedError
+    
+    trainer.fit(model, datamodule=dm)
+    trainer.test(datamodule=dm, ckpt_path='best')
+    metrics = trainer.test(datamodule=dm, ckpt_path='best')[0]
+    metric = metrics['test_metric']
+
+
+"""
+----------
+EVALUATION
+----------
+"""
+metric, test_pred, test_y = eval_model(
+    dm.train, dm.test, model, target=config['model']['target'], model_class=model_class
+)
+print(metric)
+
+print(f"Decoding results for {args.eid}: ", metric)
+res_dict = {'test_metric': metric, 'test_pred': test_pred, 'test_y': test_y}
+np.save(save_path/f'{args.eid}.npy', res_dict)
+        
